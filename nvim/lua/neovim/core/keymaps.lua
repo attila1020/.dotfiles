33ecac0bf1bccc47080e5342961544f36db984ad
attila1020 <attila.miszkuly@gmail.com>
2024-01-10 08:22:19 +0100

    Added some keymapping

--- nvim/lua/neovim/core/keymaps.lua
+++ nvim/lua/neovim/core/keymaps.lua
@@ -1,2 +1,13 @@
 -- set leader key to space
 vim.g.mapleader = " "
+
+local keymap = vim.keymap
+local opts = {noremap = true, silent = true}
+
+-- Save file while in normal and insert mode
+keymap.set("n", "<C-s>", ":w<CR>", opts)
+keymap.set("i", "<C-S>", "<Esc>:w<CR>", opts)
+
+--Lazygit
+keymap.set("n", "<leader>gg", ":LazyGit<CR>", opts)
+keymap.set("i", "<leader>gg", "<Esc>:LazyGit<CR>", opts)
