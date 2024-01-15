fafd318d29e23769113c214dbf52ba9a1704c8b5
attila1020 <attila.miszkuly@gmail.com>
2024-01-15 20:41:50 +0100

    Added keymap to exit vim

--- nvim/lua/neovim/core/keymaps.lua
+++ nvim/lua/neovim/core/keymaps.lua
@@ -1,13 +1,17 @@
 -- set leader key to space
 vim.g.mapleader = " "
 
-local keymap = vim.keymap
+local map = vim.keymap.set
 local opts = {noremap = true, silent = true}
 
 -- Save file while in normal and insert mode
-keymap.set("n", "<C-s>", ":w<CR>", opts)
-keymap.set("i", "<C-S>", "<Esc>:w<CR>", opts)
+map("n", "<C-s>", ":w<CR>", opts)
+map("i", "<C-S>", "<Esc>:w<CR>", opts)
 
 --Lazygit
-keymap.set("n", "<leader>gg", ":LazyGit<CR>", opts)
-keymap.set("i", "<leader>gg", "<Esc>:LazyGit<CR>", opts)
+map("n", "<leader>gg", ":LazyGit<CR>", opts)
+map("i", "<leader>gg", "<Esc>:LazyGit<CR>", opts)
+
+
+map("i", "<C-Q>", ":q<CR>", opts)
+map("n", "<C-Q>", ":q<CR>", opts)
