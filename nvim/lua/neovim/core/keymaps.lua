1f272aa84bb1ab96322d846929a2cf2c9f618e97
attila1020 <attila.miszkuly@gmail.com>
2024-02-27 21:22:29 +0100

    Added ESC keymap to nvim

--- nvim/lua/neovim/core/keymaps.lua
+++ nvim/lua/neovim/core/keymaps.lua
@@ -4,6 +4,11 @@ vim.g.mapleader = " "
 local map = vim.keymap.set
 local opts = {noremap = true, silent = true}
 
+--Remap ESC to jj and jk
+map("i", "jj", "<Esc>", opts)
+map("i", "jk", "<Esc>", opts)
+
+
 -- Save file while in normal and insert mode
 map("n", "<C-s>", ":w<CR>", opts)
 map("i", "<C-S>", "<Esc>:w<CR>", opts)
